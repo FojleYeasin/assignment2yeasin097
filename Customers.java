e5f1722b1afb74c5bec9b3bf1dee723f3428e91a
Yeasin <yeasin.cse19@gmail.com>
2024-06-13 12:42:09 +0600

    queue with 1 server

--- Customers.java
+++ Customers.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Customers {
     public int incoming_time;
     public int outgoing_time;
@@ -5,9 +6,12 @@ public class Customers {
     public int max_waiting_time;
     public String customerID;
     public boolean service_taken = false;
+    Random random = new Random();
 
     public Customers(int incoming_time) {
         this.incoming_time = incoming_time;
+        this.service_time =  random.nextInt(15);
+        this.max_waiting_time = random.nextInt(60);
     }
     
     public int get_waiting() {
--- Main.java
+++ Main.java
@@ -7,12 +7,13 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        Customers[] customers = new Customers[30];
+        int customer_no = 5;
+        Customers[] customers = new Customers[customer_no];
         Random random = new Random();
 
         Queue<Customers> customerQueue = new LinkedList<>();
 
-        for(int i=0; i<30; i++) {
+        for(int i=0; i<customer_no; i++) {
             int random_int = random.nextInt(100);
             //System.out.println(random_int);
             customers[i] = new Customers(random_int);
@@ -27,10 +28,29 @@ public class Main {
         
         }
 
-        
+        int running_time = 0;
         while (!customerQueue.isEmpty()) {
-            Customers currentCustomer = customerQueue.poll();
-            System.out.println("Processing " + currentCustomer.customerID + " with incoming time: " + currentCustomer.incoming_time);
+            Customers currentCustomer = customerQueue.peek();
+            if(running_time>=currentCustomer.incoming_time){
+                customerQueue.remove();
+                if(running_time < currentCustomer.incoming_time + currentCustomer.max_waiting_time) {
+                    System.out.println("Processing " + currentCustomer.customerID + " with incoming time: " + currentCustomer.incoming_time);
+                    System.out.println("Service started at " + running_time);
+                    currentCustomer.outgoing_time = running_time + currentCustomer.service_time;
+                    currentCustomer.service_taken = true;
+                    running_time += currentCustomer.service_time;
+                    continue;
+                }
+                else {
+                    System.out.println("Customer " + currentCustomer.customerID + " leaved the bank without taking service");
+                    System.out.println("Running time " + running_time);
+                    System.out.println("Incoming time " + currentCustomer.incoming_time);
+                    System.out.println("Waiting time " + currentCustomer.max_waiting_time);
+                    currentCustomer.service_taken = false;
+                }
+                
+            }
+            running_time++;
         }
     }
 }
