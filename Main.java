cb9bd24ae331929f58c234460ff0143928ba3d28
Yeasin <yeasin.cse19@gmail.com>
2024-06-13 18:20:17 +0600

    organized

--- Main.java
+++ Main.java
@@ -1,36 +1,7 @@
-import java.util.Queue;
-
 public class Main {
 
     public static void main(String[] args) {
-        int customer_no = 50;
-        QueueOfBank bankqQueue = new QueueOfBank(customer_no);
-
-        Queue<Customers> customerQueue = bankqQueue.getCustomerQueue();
-
-        int running_time = 0;
-        while (!customerQueue.isEmpty()) {
-            Customers currentCustomer = customerQueue.peek();
-            if(running_time>=currentCustomer.incoming_time){
-                customerQueue.remove();
-                if(running_time < currentCustomer.incoming_time + currentCustomer.max_waiting_time) {
-                    System.out.println("Processing " + currentCustomer.customerID + " with incoming time: " + currentCustomer.incoming_time);
-                    System.out.println("Service started at " + running_time);
-                    currentCustomer.outgoing_time = running_time + currentCustomer.service_time;
-                    currentCustomer.service_taken = true;
-                    running_time += currentCustomer.service_time;
-                    continue;
-                }
-                else {
-                    System.out.println("Customer " + currentCustomer.customerID + " leaved the bank without taking service");
-                    System.out.println("Running time " + running_time);
-                    System.out.println("Incoming time " + currentCustomer.incoming_time);
-                    System.out.println("Waiting time " + currentCustomer.max_waiting_time);
-                    currentCustomer.service_taken = false;
-                }
-                
-            }
-            running_time++;
-        }
+        Server server = new Server(50);
+        server.Serve();
     }
 }
--- Server.java
+++ Server.java
@@ -1,7 +1,45 @@
-public class Server implements Runnable{
-    
-    @Override
-    public void run(){
+import java.util.Queue;
 
+public class Server {
+    
+    private int customer_no;
+    
+    public Server(int customer_no) {
+        this.customer_no = customer_no;
+    }
+
+    
+
+
+
+    public void Serve() {
+
+        QueueOfBank bankqQueue = new QueueOfBank(customer_no);
+        Queue<Customers> customerQueue = bankqQueue.getCustomerQueue();
+
+        int running_time = 0;
+        while (!customerQueue.isEmpty()) {
+            Customers currentCustomer = customerQueue.peek();
+            if(running_time>=currentCustomer.incoming_time){
+                customerQueue.remove();
+                if(running_time < currentCustomer.incoming_time + currentCustomer.max_waiting_time) {
+                    System.out.println("Processing " + currentCustomer.customerID + " with incoming time: " + currentCustomer.incoming_time);
+                    System.out.println("Service started at " + running_time);
+                    currentCustomer.outgoing_time = running_time + currentCustomer.service_time;
+                    currentCustomer.service_taken = true;
+                    running_time += currentCustomer.service_time;
+                    continue;
+                }
+                else {
+                    System.out.println("Customer " + currentCustomer.customerID + " leaved the bank without taking service");
+                    System.out.println("Running time " + running_time);
+                    System.out.println("Incoming time " + currentCustomer.incoming_time);
+                    System.out.println("Waiting time " + currentCustomer.max_waiting_time);
+                    currentCustomer.service_taken = false;
+                }
+                
+            }
+            running_time++;
+        }
     }
 }
